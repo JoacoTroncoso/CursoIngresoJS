b311e6e198cae9d089b044c2b95819e7071efa28
Joaco Troncoso <joacotroncoso.02@gmail.com>
2021-02-03 18:59:35 -0600

    nuevo cambio

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -5,21 +5,71 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	alert("ok");	
+ let num1;
+ let num2;
+ let sumar;
+
+ num1 = document.getElementById("txtIdNumeroUno").value;
+ num1 = parseInt(num1);
+
+ num2 = document.getElementById("txtIdNumeroDos").value;
+ num2 = parseInt(num2);
+
+ sumar = num1 + num2;
+
+ alert("La suma es " + sumar);
+
 }
 
 function restar()
 {
-	
+	let num1;
+	let num2;
+	let restar;
+
+	num1 = document.getElementById("txtIdNumeroUno").value;
+	num1 = parseInt(num1);
+
+	num2 = document.getElementById("txtIdNumeroDos").value;
+	num2 = parseInt(num2);
+
+	restar = num1 - num2;
+
+	alert("La resta es " + restar);
 }
 
 function multiplicar()
 { 
-	
+	let num1;
+	let num2;
+	let multiplicar;
+
+	num1 = document.getElementById("txtIdNumeroUno").value;
+	num1 = parseInt(num1);
+
+	num2 = document.getElementById("txtIdNumeroDos").value;
+	num2 = parseInt(num2);
+
+	multiplicar = num1 * num2;
+
+	alert("La multiplicación es " + multiplicar)
 }
 
 function dividir()
 {
-	
+	let num1;
+	let num2;
+	let dividir;
+
+	num1 = document.getElementById("txtIdNumeroUno").value;
+	num1 = parseInt(num1);
+
+	num2 = document.getElementById("txtIdNumeroDos").value;
+	num2 = parseInt(num2);
+
+	dividir = num1 / num2;
+
+	alert("La división es " + dividir);
+
 }
 
